3b02845661b60a70b8740f77468edba9c1c188c8
irenek <frybitsinc@gmail.com>
2022-01-28 12:29:06 +0900

    first commit

---
+++ app/src/main/java/com/frybitsinc/myebook/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.frybitsinc.myebook.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
