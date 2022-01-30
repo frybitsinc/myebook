a1d21f9cf1d74761b896633d31feec6a10589c02
irenek <frybitsinc@gmail.com>
2022-01-30 22:04:23 +0900

    add model BookItem

---
+++ app/src/main/java/com/frybitsinc/myebook/model/BookItem.kt
@@ -0,0 +1,12 @@
+package com.frybitsinc.myebook.model
+
+data class BookItem(
+        var title: String = "",
+        var link: String = "",
+        var image: String = "",
+        var author: String = "",
+        var price: String = "",
+        var publisher: String = "",
+        var pubdate: String = "",
+        var isbn: String = ""
+)
--- app/src/main/java/com/frybitsinc/myebook/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/frybitsinc/myebook/ui/home/HomeViewModel.kt
@@ -9,5 +9,10 @@ class HomeViewModel : ViewModel() {
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
     }
+    private val _subtitle = MutableLiveData<String>().apply {
+        value = "Subtitle"
+    }
+
     val text: LiveData<String> = _text
+    val subtitle: LiveData<String> = _subtitle
 }
