461b6c9293d1cc7bb22709b4ec7580d74e6703b3
irenek <frybitsinc@gmail.com>
2022-01-31 00:42:57 +0900

    get json

--- app/src/main/java/com/frybitsinc/myebook/api/BookApi.kt
+++ app/src/main/java/com/frybitsinc/myebook/api/BookApi.kt
@@ -2,8 +2,15 @@ package com.frybitsinc.myebook.api
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Headers
 
 interface BookApi {
-    @GET("/v1/search/book_adv")
+    @Headers(
+        "X-Naver-Client-Id: 4PlWWzVp448z2YrYDa3S",
+        "X-Naver-Client-Secret: GR0rkuQsTQ",
+    )
+    @GET("/v1/search/book.json?"+
+            "sort=date"+
+            "&query=안드로이드")
     fun fetchBooks(): Call<String>
 }
